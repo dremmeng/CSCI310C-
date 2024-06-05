9ab631cd4c697c352e604bb98c0462dc0e271d65
dremmeng <44462821+dremmeng@users.noreply.github.com>
2024-06-05 16:30:38 -0600

    Add files via upload

---
+++ HW1Types.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace HW1types
+{
+    class HW1types
+    {
+        static void Main()
+        {
+            int a = 1;
+            char b = 'a';
+            decimal c = 1;
+            string d = "string";
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            a = 2;
+            b = 'c';
+            c = 2;
+            d = "New";
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            double length1 = 1;
+            double length2 = 1;
+            double length3 = 1;
+            double s = length1 + length2 + length3;
+            double area = Math.Sqrt(s * (s - length1) * (s - length2) * (s -= length3));
+            Console.WriteLine(area);
+
+        }
+    }
+}
