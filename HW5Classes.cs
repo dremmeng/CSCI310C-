44cdefd69b781b5aae9377b73e480aca6f839551
dremmeng <44462821+dremmeng@users.noreply.github.com>
2024-06-07 09:06:26 -0600

    Add files via upload

--- HW5Classes.cs
+++ HW5Classes.cs
@@ -1,23 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
 namespace HwClasses
 {
-    class Person
+    public class Person
     {
         private string name;
         private int age;
-
-        public Person(string _name, int _age)
+        public void SetAge(int _age)
         {
-            this.name = _name;
             this.age = _age;
         }
+        public void SetName(string _name) 
+        {
+            this.name = _name;
+        }
+        public int GetAge()
+        {
+            return this.age;
+        }
+        public string GetName() 
+        {
+            return this.name;
+        }
+
     }
     class HW5Classes
     {
         public static void Main()
         {
-            Person person1 = new Person("Kevin", 12);
-            Person person2 = new Person("Steve", 15);
+            Person person1 = new Person();
+            person1.SetAge(15);
+            person1.SetName("Kevin");
+            Person person2 = new Person();
+            person2.SetAge(12);
+            person2.SetName("Steve");
+            Console.WriteLine("Person 1's name is " + person1.GetName());
+            Console.WriteLine("Person 2's name is " + person2.GetName());
+            Console.WriteLine("Person 1's age is " + person1.GetAge());
+            Console.WriteLine("Person 2's age is " + person2.GetAge());
+
         }
     }
 }
