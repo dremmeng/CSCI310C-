a9d7eb1855adf2d2038112d38d630f5616c167a3
dremmeng <44462821+dremmeng@users.noreply.github.com>
2024-06-07 08:37:34 -0600

    Add files via upload

---
+++ HW5Classes.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HwClasses
+{
+    class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string _name, int _age)
+        {
+            this.name = _name;
+            this.age = _age;
+        }
+    }
+    class HW5Classes
+    {
+        public static void Main()
+        {
+            Person person1 = new Person("Kevin", 12);
+            Person person2 = new Person("Steve", 15);
+        }
+    }
+}
