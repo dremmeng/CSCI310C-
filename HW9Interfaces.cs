979a466603c297ee7c78aeef67939ab9940c411c
dremmeng <44462821+dremmeng@users.noreply.github.com>
2024-06-11 12:27:17 -0600

    Add files via upload

---
+++ HW9Interfaces.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HW9Interfaces
+{
+    interface animalSound
+    {
+        void animalSound(); // interface method
+    }
+
+    interface classification
+    {
+        void classification(); // interface method
+    }
+
+    // Implement multiple interfaces
+    class Dog : animalSound, classification
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("Dog goes bark");
+        }
+        public void classification()
+        {
+            Console.WriteLine("Dogs are mammals");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.animalSound();
+            dog.classification();
+        }
+    }
+}
