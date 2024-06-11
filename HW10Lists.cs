b77862889c7017013138e4b21f8d9eb3640e8f6e
dremmeng <44462821+dremmeng@users.noreply.github.com>
2024-06-11 12:15:14 -0600

    Add files via upload

---
+++ HW10Lists.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace HW10Lists
+{
+    public class HW10lists
+    { 
+        static void binaryInsert(List<int> list1, int i)
+        {
+            int index = list1.BinarySearch(i);
+            if (index == -1)
+            {
+                index = 0;
+            }
+            list1.Insert(index, i);
+        }
+        public static void Main()
+        {
+
+            List<Double> list2 = new List<Double>();
+            int index = 0;
+            Random rnd = new Random();
+            List<int> list1 = new List<int>();
+            while (true)
+            {
+                int randomNum = rnd.Next(11, 100);
+                list1.Add(randomNum);
+                if (index == 10)
+                {
+                    break;
+                }
+                index++;
+            }
+            while (true)
+            {
+                double randomNum = rnd.NextDouble();
+                list2.Add(randomNum);
+                if (index == 10)
+                {
+                    break;
+                }
+                index++;
+            }
+
+            Console.WriteLine("Enter number to insert:");
+            int i = (int)Single.Parse(Console.ReadLine());
+            list1.Sort();
+            binaryInsert(list1, i);
+        }
+    }
+    
+}
