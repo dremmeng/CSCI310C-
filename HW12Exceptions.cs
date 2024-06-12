dec0d00bc4b0604163e5bfc3a47916e57b3be0c1
dremmeng <44462821+dremmeng@users.noreply.github.com>
2024-06-12 08:17:35 -0600

    Add files via upload

---
+++ HW12Exceptions.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace HW12Exceptions
+{
+    public class InvalidAgeException : Exception
+    {
+        public InvalidAgeException(String message)
+            : base(message)
+        {
+
+        }
+    }
+    public class TestUserDefinedException
+    {
+        static void validate(int age)
+        {
+            if (age > 120)
+            {
+                throw new InvalidAgeException("Sorry, Age must be less than 120");
+            }
+        }
+        public static void Main()
+        {
+            try
+            {
+                validate(12);
+            }
+            catch (InvalidAgeException e) { Console.WriteLine(e); }
+            try
+            {
+                validate(130);
+            }
+            catch (InvalidAgeException e) { Console.WriteLine(e); }
+
+        }
+    }
+}
