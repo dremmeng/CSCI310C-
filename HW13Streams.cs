e853647801d745817d851cbd40b45f40861f8acb
dremmeng <44462821+dremmeng@users.noreply.github.com>
2024-06-13 09:56:58 -0600

    Add files via upload

---
+++ HW13Streams.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using System.IO;
+
+namespace HW13Streams
+
+{
+
+    class HW13Streams
+
+    {
+
+        static void Main()
+
+        {
+
+            string readPath = @"C:\Users\Drewr\Desktop\in.txt";
+
+            string writePath = @"C:\Users\Drewr\Desktop\out.txt";
+
+            try
+
+            {
+
+                string[] lines = File.ReadAllLines(readPath);
+
+                using (StreamWriter sw = File.AppendText(writePath))
+
+                {
+
+                    foreach (string line in lines)
+
+                    {
+
+                        sw.WriteLine(line.ToUpper());
+
+                    }
+
+                }
+
+            }
+
+            catch (IOException e)
+
+            {
+
+                Console.WriteLine("An error occurred");
+
+                Console.WriteLine(e.Message);
+
+            }
+
+        }
+
+    }
+
+}
